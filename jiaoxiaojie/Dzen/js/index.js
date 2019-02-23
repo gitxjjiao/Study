2c8d3abebcb840202205ab5801c307930761a800
gitxjjiao <1505922655@qq.com>
2019-02-23 16:15:29 +0800

    完成了Blog页面，修改了一小动画，细节。

--- jiaoxiaojie/Dzen/js/index.js
+++ jiaoxiaojie/Dzen/js/index.js
@@ -2,39 +2,49 @@
             
             $(document).ready(function(){
              $("#all").click(function(){
-                  $("#ui1").slideToggle("slow");
-                  $("#ui2").slideToggle("slow");
-                  $("#ui3").slideToggle("slow");
-                  $("#ui4").slideToggle("slow");
-                  $("#ui5").slideToggle("slow");
-                  $("#ui6").slideToggle("slow");
-                  $("#ui7").slideToggle("slow");
-                  $("#ui8").slideToggle("slow");
+                 console.log("12222")
+                  $(".photo").css("display","block");
+                 $(".photo").slideDown("slow");
+                  $(".ide").css("display","none");
+                     $(".ui").css("display","none");
+                     $(".il").css("display","none");
+                       $(".br").css("display","none");
+                  
+                  
                  
                  });
                 $("#ide").click(function(){
-                   $("#ui2").slideToggle("slow");
-                   $("#ui3").slideToggle("slow");
-                   $("#ui4").slideToggle("slow");
-                    $("#ui1").css.style("dispaly","none");
-                    $("#ui5").css.style("dispaly","none");
-                    $("#ui6").css.style("dispaly","none");
-                    $("#ui7").css.style("dispaly","none");
-                    $("#ui8").css.style("dispaly","none");
+                   $(".ide").css("display","block");
+                   $(".ide").slideDown("slow");
+                    $(".photo").css("display","none");
+                      $(".ui").css("display","none");
+                      $(".il").css("display","none");
+                        $(".br").css("display","none");
                  });
              $("#uii").click(function(){
-               $("#ui2").slideToggle("slow");
-               $("#ui3").slideToggle("slow");
-               $("#ui4").slideToggle("slow");
+                $(".ui").css("display","block");
+                $(".ui").slideDown("slow");
+                 $(".il").css("display","none");
+                  $(".br").css("display","none");
+                  $(".ide").css("display","none");
+                   $(".photo").css("display","none");
+                
               }); 
               $("#ill").click(function(){
-                 $("#ui4").slideToggle("slow");
-                 $("#ui5").slideToggle("slow");
-                 $("#ui6").slideToggle("slow");
+                  $(".il").css("display","block");
+                  $(".il").slideDown("slow");
+                   $(".ui").css("display","none");
+                    $(".br").css("display","none");
+                    $(".ide").css("display","none");
+                     $(".photo").css("display","none");
+                  
                  });
              $("#brd").click(function(){
-               $("#ui6").slideToggle("slow");
-               $("#ui7").slideToggle("slow");
-               $("#ui8").slideToggle("slow");
+                $(".br").css("display","block");
+                $(".br").slideDown("slow");
+                 $(".il").css("display","none");
+                 $(".ui").css("display","none");
+                  $(".ide").css("display","none");
+                   $(".photo").css("display","none");
               }); 
                }); 
